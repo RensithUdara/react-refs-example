fdf44e12525bc347dc0814076b0e31a4020a3f7d
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-19 01:11:40 +0530

    To pass a ref to a child component use forwardRef

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { useRef } from 'react'
+import MyInput from './components/MyInput';
 
 function App() {
   const inputRef = useRef(null);
 
   const handleClick = () => {
-    console.log(inputRef.current.value);
+    inputRef.current.focus();
+    //    console.log(inputRef.current.value);
   };
 
   return (
     <div className="App">
-      <input type='text' ref={inputRef} />
+      <MyInput ref={inputRef} />
+      {/* <input type='text' ref={inputRef} /> */}
       <button onClick={handleClick}>Focus</button>
     </div>
   );
